ad2bcc53c408e86fead6e1d61a1f4f76df6a1f1d
phong <phongndsaiyan@gmail.com>
2023-04-22 16:26:03 +0700

    addLayer and compile function

--- VGG16.py
+++ VGG16.py
@@ -1,7 +1,19 @@
-
 import numpy as np
+from threading import Thread
 
-
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+    
 def ReLU(input):
     return np.maximum(input, 0)
 def leaky_ReLU(input):
@@ -66,6 +78,7 @@ def fftZ_conv(X, K, B):
             sum = 0
             for k in range (X.shape[1]):    
                 sum += fft_conv(X[i][k],K[j][k])
+            # print(B[j].shape)
             output[i][j] = sum + B[j]   
     return output
 def conv_3d(input, kernel, kernel_size, n_kernels):
@@ -75,8 +88,7 @@ def conv_3d(input, kernel, kernel_size, n_kernels):
             output[i][k] = fft_conv(input[k], kernel[i])
     return output
 def dK_convolution(X, dZ, K_shape):                                              
-    dZ_size = dZ.shape[-1]
-    dK = np.zeros((dZ.shape[1], X.shape[1],K_shape[2], K_shape[3]))
+    dK = np.zeros(K_shape)
     for i in range (X.shape[0]):
         dK += np.fft.ifft2(conv_3d(X[i], dZ[i], K_shape[2],K_shape[0]), s = K_shape[2:]).real
     return dK
@@ -90,16 +102,21 @@ def dX_convolution(dZ, K):
     return dX
 
 def max_pooling(input, pool_size = 2):
+    m, n = input.shape
     pool_stride = pool_size
-    conv2 = np.zeros((1, (input.shape[1] - pool_size)//pool_stride + 1))
-    for i in range(0,input.shape[0] - pool_size + 1, pool_stride):
-        row = np.array([])
-        for k in range(0,input.shape[1] - pool_size + 1, pool_stride):
-            res = np.max(input[i : i + pool_size,k : k + pool_size])
-            row = np.append(row, res).reshape(1, -1)
-        conv2 = np.vstack((conv2, row))
-    conv2 = conv2[1:,:]
-    return conv2
+    res = input.reshape(m//pool_size,pool_stride, n//pool_size, pool_stride).max((1,3))
+    return res
+# def max_pooling(input, pool_size = 2):
+#     pool_stride = pool_size
+#     conv2 = np.zeros((1, (input.shape[1] - pool_size)//pool_stride + 1))
+#     for i in range(0,input.shape[0] - pool_size + 1, pool_stride):
+#         row = np.array([])
+#         for k in range(0,input.shape[1] - pool_size + 1, pool_stride):
+#             res = np.max(input[i : i + pool_size,k : k + pool_size])
+#             row = np.append(row, res).reshape(1, -1)
+#         conv2 = np.vstack((conv2, row))
+#     conv2 = conv2[1:,:]
+#     return conv2
 def avg_pooling(input, pool_size = 2):
     pool_stride = pool_size
     conv2 = np.zeros((1, (input.shape[1] - pool_size)//pool_stride + 1))
@@ -150,16 +167,23 @@ def pool_de_3d(dP, P, C, pool_size, pool_de):
     return np.array(res)
 
 class Conv_layer:
-    def __init__(self, X_shape, kernel_size, n_kernels):
+    def __init__(self, kernel_size, n_kernels):
         self.n_kernels = n_kernels
         self.kernel_size = kernel_size
-        self.X_shape = X_shape
-        self.output_shape = (X_shape[0], n_kernels, X_shape[2], X_shape[3])
-        self.kernel_shape = (n_kernels, X_shape[1],kernel_size, kernel_size)
-        self.K = np.random.randn(*self.kernel_shape)
-        self.B = np.random.randn(n_kernels, self.output_shape[2], self.output_shape[3])
         self.dK = None
         self.dB = None
+    def compile(self, X_shape):
+        self.X_shape = X_shape
+        self.output_shape = (X_shape[0], self.n_kernels, X_shape[2], X_shape[3])
+        self.kernel_shape = (self.n_kernels, X_shape[1],self.kernel_size, self.kernel_size)
+        fan_in = X_shape[1] * X_shape[2] * X_shape[3]
+        std = np.sqrt(6 / fan_in)
+        self.K = np.random.normal(loc = 0, scale=std, size=self.kernel_shape)
+        # fan_in = X_shape[1] * self.kernel_size * self.kernel_size
+        # fan_out = self.kernel_shape[1] * self.kernel_size * self.kernel_size
+        # std = np.sqrt(2 / (fan_in + fan_out))
+        # self.K = np.random.normal(loc = 0, scale=std, size=self.kernel_shape)
+        self.B = np.random.randn(self.n_kernels, self.output_shape[2], self.output_shape[3])
     def forward(self, X):
         self.X = X
         self.dX = np.copy(X)
@@ -176,36 +200,42 @@ class Conv_layer:
         self.K -= learning_rate * self.dK
 
 class Pool_layer:
-    def __init__(self, input_shape, pool_size, pool_type = 'max'):
+    def __init__(self, pool_size, pool_type = 'max'):
+        self.P = None
+        self.C = None
         self.pool_size = pool_size
         self.input = None
-        self.output_shape = (input_shape[0], input_shape[1], (input_shape[2] - pool_size) // pool_size + 1, (input_shape[3] - pool_size) // pool_size + 1)
-        self.output = np.zeros(self.output_shape)
+        # self.output_shape = (input_shape[0], input_shape[1], (input_shape[2] - self.pool_size) // self.pool_size + 1, (input_shape[3] - self.pool_size) // self.pool_size + 1)
         if pool_type == 'max':
             self.pool = max_pooling
             self.pool_de = maxpool_de
         else:
             self.pool = avg_pooling
             self.pool_de = avgpool_de
-    def forward(self, input):
-        for i in range (self.output_shape[0]):
-            self.output[i] = pool_3d(input[i], self.pool_size, self.pool)
-        return self.output
-    def backward(self, dP, P, C):
-        res = []
-        for i in range (self.output_shape[0]):
-            res.append(pool_de_3d(dP[i], P[i], C[i], self.pool_size, self.pool_de))
-        return np.array(res)
 
-class Reshape:
-    def __init__(self, input_shape, output_shape):
-        self.input_shape = input_shape
-        self.output_shape = output_shape
     def forward(self, input):
-        return input.reshape(self.output_shape)
+        self.C = input
+        self.P = np.zeros((input.shape[0], input.shape[1], (input.shape[2] - self.pool_size) // self.pool_size + 1, (input.shape[3] - self.pool_size) // self.pool_size + 1))
+        for i in range (input.shape[0]):
+            self.P[i] = pool_3d(input[i], self.pool_size, self.pool)
+        return self.P
+    def backward(self, dP):
+        res = []
+        for i in range (self.C.shape[0]):
+            res.append(pool_de_3d(dP[i], self.P[i], self.C[i], self.pool_size, self.pool_de))
+        return np.array(res)
+    def update(self, learning_rate):
+        return None
+class Reshape:
+    def __init__(self):
+        return None
+    def forward(self, input):
+        self.input_shape = input.shape
+        return input.reshape(input.shape[1] * input.shape[2] * input.shape[3],input.shape[0])
     def backward(self, output):
         return output.reshape(self.input_shape)
-
+    def update(self, learning_rate):
+        return None
 class FcSub_Layer:
     def __init__(self, row, col, act_func, isoutputlayer = False):
         self.isoutputlayer = isoutputlayer
@@ -238,20 +268,26 @@ class FcSub_Layer:
         self.bias -= learning_rate * self.db 
 
 class FullyConnected:
-    def __init__(self, n_nodes_list, hiddenlayers_actfunc):
+    def __init__(self, hidden_layers_ls, output_dim, hiddenlayers_actfunc):
         self.pred_his = []
         self.layers = []
-        for i in range (len(n_nodes_list) - 1):
-            self.layers.append(FcSub_Layer(row = n_nodes_list[i], col=n_nodes_list[i + 1], act_func=hiddenlayers_actfunc))
+        self.dim_layers_ls = hidden_layers_ls
+        self.output_dim = output_dim
+        self.hiddenlayers_actfunc = hiddenlayers_actfunc
+        self.cost_his = []
+    def compile(self, input_dim):
+        self.dim_layers_ls.insert(0, input_dim)
+        self.dim_layers_ls.append(self.output_dim)
+        for i in range (len(self.dim_layers_ls) - 1):
+            self.layers.append(FcSub_Layer(row = self.dim_layers_ls[i], col=self.dim_layers_ls[i + 1], act_func=self.hiddenlayers_actfunc))
         self.layers[-1].activationForward = Soft_Max
         self.layers[-1].activationBackward = Soft_Max_de
-        self.cost_his = []
     def forward(self, input):
         x = np.copy(input)
         for layer in self.layers:
             x = layer.forward(x)
         return x
-    def backward(self, target, predict):
+    def backward(self, predict, target):
         delta = predict - target
         for layer in reversed(self.layers):
             d_input = delta * layer.activationBackward(layer.output)
@@ -274,43 +310,52 @@ class FullyConnected:
         return predict
 
 class CNN:
-    def __init__(self, input_shape, output_shape):
-        self.conv = Conv_layer(input_shape, 5, 5)
-        self.pool = Pool_layer(self.conv.output_shape, 4, 'max')
-        self.rs = Reshape(self.pool.output_shape, (self.pool.output_shape[1] * self.pool.output_shape[2] * self.pool.output_shape[3], input_shape[0]))
-        self.flc = FullyConnected([self.rs.output_shape[0],10, 10, output_shape[0]], Tanh)
+    def __init__(self, input_shape):
+        self.input_shape = list(input_shape)
+        self.layers = []
         self.cost_his = []
+        self.val_cost_his = []
+    def addLayer(self,layer):
+        self.layers.append(layer)
+    def compile(self):
+        flc_input_dim = 1
+        self.layers[0].compile(self.input_shape)
+        dim_3d = self.layers[0].n_kernels
+        for layer in self.layers[1:]:
+            if layer.__class__.__name__ == "Pool_layer":
+                flc_input_dim *= layer.pool_size
+                # self.input_shape[2] //= flc_input_dim 
+                # self.input_shape[3] //= flc_input_dim
+            if layer.__class__.__name__ == "Conv_layer":
+                layer.compile((self.input_shape[0], dim_3d, self.input_shape[2] // flc_input_dim,self.input_shape[3] // flc_input_dim))
+                dim_3d = layer.n_kernels
+        self.layers[-1].compile((self.input_shape[2] // flc_input_dim) * (self.input_shape[3] // flc_input_dim) * dim_3d)
+
     def forward(self, input):
-        C = self.conv.forward(input)
-        P = self.pool.forward(C)
-        f = self.rs.forward(P)
-        predict = self.flc.forward(f)
-        return predict, C, P
-    def backward(self, predict,target,  P, C):
-        df = self.flc.backward(predict=predict, target=target)
-        dP = self.rs.backward(df)
-        dC = self.pool.backward(dP, P, C)
-        dX = self.conv.backward(dC)
-        return dX
+        predict = self.layers[0].forward(input)
+        for layer in self.layers[1:]:
+            predict = layer.forward(predict)
+        return predict
+    def backward(self, predict,target):
+        delta = self.layers[-1].backward(predict, target)
+        for layer in reversed (self.layers[:-1]):
+            delta = layer.backward(delta)
     def update(self, learning_rate = 0.1):
-        self.flc.update(learning_rate)
-        self.conv.update(learning_rate)
-    def train(self,input, target,input_val, target_val, learning_rate, epochs,lr_down = True, lr_decay = 20):
+        for layer in self.layers:
+            layer.update(learning_rate)
+    def train(self,input, target, input_val, target_val, learning_rate, epochs,lr_down = True, lr_decay = 20):
+        fit_range = [1,2,3,4,5,6,7,8,9,10]
         for i in range (epochs):
-            predict, C, P = self.forward(input)
+            predict = self.forward(input)
             if lr_down == True:
                 if i%100==0:
                     learning_rate = learning_rate - learning_rate/lr_decay
-            self.backward(predict, target, P, C)
+            self.backward(predict, target)
             self.update(learning_rate)
             self.cost_his.append(CrossEntropy(predict, target))
+            predict_val= self.forward(input_val)
+            self.val_cost_his.append(CrossEntropy(predict_val, target_val))
+            if (i + 1) * 10 / (epochs) in fit_range:
+                print(f"Loading {(i + 1) * 100 / (epochs)}%")
+        print("Learning process completed!!!")
             
-
-
-
-
-
-
-
-
-
